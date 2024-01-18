f84cc307a427b9e6feafb178d90dae5398a13f8d
jeferson <jefersonb88@gmail.com>
2024-01-18 10:59:50 -0300

    recarregamento auto do iframe

--- src/script/errosDeCarregamento.js
+++ src/script/errosDeCarregamento.js
@@ -2,3 +2,11 @@ function iframeErro() {
     console.log("Erro ao carregar a iframe!");
     document.getElementById('iframeMaps').innerHTML = 'Erro ao carregar a imagem!';
 }
+
+function recarregarIframe() {
+    var iframe = document.getElementById('iframeMaps');
+    iframe.src = iframe.src; // Esta linha força o recarregamento do iframe
+}
+
+recarregarIframe();
+
